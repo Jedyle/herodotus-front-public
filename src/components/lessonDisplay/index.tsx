f1f2df8900b085879db49fe4e1e732a32591c618
Jérémy <jeremy.lixandre@gmail.com>
2022-08-21 18:27:16 +0200

    chg: :art: try new cfg

--- src/components/lessonDisplay/index.tsx
+++ src/components/lessonDisplay/index.tsx
@@ -34,8 +34,8 @@ const LessonDisplay: React.FC<LessonDisplayProps> = ({lesson}) => {
   }  
 
   return (
-    <div style={{height: "calc(100% - 56px)", display: "flex", flexDirection: "column", marginLeft: "10px", marginRight: "10px"}}>
-      <div style={{flexGrow: 1, height: "calc(100% - 56px)", overflow: "scroll"}} dangerouslySetInnerHTML={{__html: lesson.slides[currentSlide].content}}></div>
+    <div style={{height: "100%", display: "flex", flexDirection: "column", marginLeft: "10px", marginRight: "10px"}}>
+      <div style={{flexGrow: 1, height: "100%", overflow: "scroll"}} dangerouslySetInnerHTML={{__html: lesson.slides[currentSlide].content}}></div>
       <IonGrid>
 	<IonRow>
 	  <IonCol size="6">

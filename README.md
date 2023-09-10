198ea2386b4d2e5f6ecfea71021d7458b3a9be61
Jedyle <jeremy.lixandre@protonmail.com>
2023-09-10 11:38:28 +0200

    chg: changes in android.xml

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 ## Generate icons
 
 	npx capacitor-assets generate
+
+## Deploy Android
+
+sync android
+
+	npx ionic cap sync
+
+generate assets (splash screen, icon)
+
+	npx capacitor-assets generate
+
+go to android studio 
+
+	npx cap open android
+	
+generate app bundle
+
+
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,30 +3,29 @@ import { useIonViewWillEnter } from '@ionic/react';
 import { getAboutPage } from 'services/api';
 import Page from 'pages/Page';
 
-const AboutPage: React.FC =  () => {
- 
-  const [article, setArticle] = useState<any>({});
-  
-  useIonViewWillEnter(() => {
-    getAboutPage().then((response: any) => {
-      setArticle(response.data)
-    }).catch(() => {})
-    
-  })
-  
-  return (
-    <Page
-      key={"about"}
-      name={"About"}
-      content={
-	<div style={{margin: "20px"}}>
-	  <h1>About this app</h1>
-	  <div dangerouslySetInnerHTML={{__html: article.article}} ></div>
-	</div>
-      }
-    />
-  )
+const AboutPage: React.FC = () => {
+
+    const [article, setArticle] = useState<any>({});
+
+    useIonViewWillEnter(() => {
+        getAboutPage().then((response: any) => {
+            setArticle(response.data)
+        }).catch(() => { })
+
+    })
+
+    return (
+        <Page
+            key={"about"}
+            name={"About"}
+            content={
+                <div style={{ margin: "20px" }}>
+                    <h1>About this app</h1>
+                    <div dangerouslySetInnerHTML={{ __html: article.article }} ></div>
+                </div>
+            }
+        />
+    )
 }
 
 export default AboutPage;
-
